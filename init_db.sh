e3d86765b82963746256e9d2ffd7bd97b0d9663c
David Lukes <dafydd.lukes@gmail.com>
2015-06-05 10:39:26 +0200

    add NFD normalization to init db script

--- init_db.sh
+++ init_db.sh
@@ -14,17 +14,22 @@ fi
 if [[ -z $1 ]]; then
     tsv=achsynku.tsv
 else
-    tsv=$1
+    tsv="$1"
 fi
 
+echo "NFD-normalizing input .tsv: $tsv..."
+perl -i -CSAD -MUnicode::Normalize -nle 'print NFD $_' "$tsv"
+
+echo "Removing old achsynku.sqlite databse..."
 rm -f achsynku.sqlite
 
-cat <<EOF | sqlite3 achsynku.sqlite && exit 0
+echo "Creating new achsynku.sqlite database based on $tsv..."
+cat <<EOF | sqlite3 achsynku.sqlite && echo "Done." && exit 0
 create table word2lemma(id int primary key,
                         word text collate nocase,
                         lemma text collate nocase);
 .separator "\t"
-.import $tsv word2lemma
+.import "$tsv" word2lemma
 create index word_index on word2lemma (word);
 create index lemma_index on word2lemma (lemma);
 EOF
